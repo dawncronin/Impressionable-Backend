4827cbf48c63fa2b01ca6ef828583ca40c12b76d
Mustafa Muse <umpp101@icloud.com>
2019-12-20 11:41:54 -0800

    adding some gems and faker seed to test all the relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+10.times do
+  Celebrity.create!(
+    name: Faker::TvShows::FamilyGuy.character,
+    description: Faker::Quote.famous_last_words,
+    image: Faker::Avatar.image(slug: "my-own-slug", size: "50x50"),
+    audio_text: Faker::TvShows::FamilyGuy.quote,
+  )
+end
+
+10.times do
+  User.create!(
+    username: Faker::Internet.username,
+  )
+end
+
+10.times do
+  Impression.create!(
+    user_id: rand((User.all.first.id)..(User.all.last.id)),
+    celebrity_id: rand((Celebrity.all.first.id)..(Celebrity.all.last.id)),
+    match_score: Faker::Number.between(from: 1, to: 99),
+  )
+end
+
+10.times do
+  Like.create!(
+    user_id: rand((User.all.first.id)..(User.all.last.id)),
+    impression_id: rand((Impression.all.first.id)..(Impression.all.last.id)),
+  )
+end
+
