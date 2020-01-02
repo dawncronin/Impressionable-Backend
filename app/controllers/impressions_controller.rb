f9adda9ea3105b54ccfba251237825486b27d72a
dawncronin <github email address>
2020-01-02 14:11:37 -0800

    added scarface seed

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -7,20 +7,19 @@ class ImpressionsController < ApplicationController
 
     def create
         impression = Impression.create!(impression_params)
-        impression.audio.attach(impression_params[:audio])
-        render json: impression
-        # render json: ImpressionSerializer.new(impression)
+        impression.audio_impression.attach(impression_params[:audio_impression])
+        # render json: impression
+        render json: {id: impression.id, link: url_for(impression.audio_impression), user_id: impression.user.id, username: impression.user.username}
     end
 
     def audio
         impression = Impression.find(params[:id])
-
-       render json: {link: url_for(impression.audio_impression), user_id: impression.user.id, username: impression.user.username}
+        render json: {id: impression.id, link: url_for(impression.audio_impression), user_id: impression.user.id, username: impression.user.username}
     end
 
     private
 
     def impression_params
-        params.require(:impression).permit(:user_id, :celebrity_id, :audio, :match_score)
+        params.require(:impression).permit(:user_id, :celebrity_id, :audio_impression, :match_score)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,24 @@ require "faker"
   chewy = Celebrity.create(
     name: "Chewbacca",
     description: "Star Wars Character",
-    image: Faker::Avatar.image(size: "10x10"),
+    image: "https://pmcvariety.files.wordpress.com/2018/05/unnamed10.jpg?w=700&h=393&crop=1",
     audio_text: "Wawaaawawa",
   )
   chewy.audio.attach(io: File.open('../chewbacca.mp3'), filename: 'chewbacca.mp3')
 
   puts chewy.audio_url = chewy.audio_url
   chewy.save
+
+  scarface = Celebrity.create(
+    name: "Scarface",
+    description: "1983 film about a miami drug lord",
+    image: "https://m.media-amazon.com/images/M/MV5BMjI4NDI0NjM5MF5BMl5BanBnXkFtZTcwMDM3NDE0Ng@@._V1_SY1000_CR0,0,1412,1000_AL_.jpg",
+    audio_text: "Say hello to my little friend",
+  )
+  scarface.audio.attach(io: File.open('../scarface.mp3'), filename: 'scarface.mp3')
+
+  puts scarface.audio_url = scarface.audio_url
+  scarface.save
 
 
 
